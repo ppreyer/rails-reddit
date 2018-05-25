d9fb1b9a0688145571aa641076561c6e641c91f6
Parker <parker.preyer1@gmail.com>
2018-05-25 12:30:23 -0400

    adds login, sessions, and includes bootstrap

--- rails-reddit/app/controllers/application_controller.rb
+++ rails-reddit/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+   # Allows you to use current user in the views
+   helper_method :current_user
+   private
+ 
+   def current_user    
+     # if the session is not empty...
+     if session[:user_id]
+       # if I have the variable @user use that if not set it to this...
+       @user ||= User.find(session[:user_id])
+       return @user
+     end
+   end
 end
--- rails-reddit/app/controllers/logins_controller.rb
+++ rails-reddit/app/controllers/logins_controller.rb
@@ -1,2 +1,26 @@
 class LoginsController < ApplicationController
+  def index
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by(username: params[:name])
+    if user and user.authenticate(params[:password])
+      # Setting a session to specific user.id
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "You have successfully logged in!"
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_login_path, notice: "You have successfully logged out!"
+  end
+
 end
