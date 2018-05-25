50e1f05dcc323f273eb05d42b07d0a19257a7959
Crystal Free <cdmackeyfree@gmail.com>
2018-05-25 15:33:18 -0400

    adds posts crud actions

--- rails-reddit/app/controllers/posts_controller.rb
+++ rails-reddit/app/controllers/posts_controller.rb
@@ -3,6 +3,50 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    # @post = Post.new
+  end
+
   def create
+    # if current_user
+      @post = Post.new(post_params)
+      # if
+        @post.save
+        redirect_to posts_path, notice: "Your post has been saved!"
+      # else 
+      #   render 'new', notice: "Try again!"
+      # end
+    # else
+    #   redirect_to logins_path, notice: "You must be logged in to post!"
+    # end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :title, :content)
   end
 end
