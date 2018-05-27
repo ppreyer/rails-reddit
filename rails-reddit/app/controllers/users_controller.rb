4b567774d28cfdeee52820436819613f86c80091
Parker <parker.preyer1@gmail.com>
2018-05-27 13:16:13 -0400

    add edit user info feature

--- rails-reddit/app/controllers/users_controller.rb
+++ rails-reddit/app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user
   
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
@@ -12,6 +15,20 @@ class UsersController < ApplicationController
       redirect_to new_login_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      redirect_to posts_path
+    else
+      render 'edit'
     end
   end
 
